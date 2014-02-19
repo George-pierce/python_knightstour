7027ee6180f4cea337e145be76efe03bbfe3be17
George-pierce <zephon.pierce@gmail.com>
2014-02-19 00:09:42 -0500

    added knights moves

--- KnightsTour/KnightsTour.py
+++ KnightsTour/KnightsTour.py
@@ -1,1 +1,14 @@
-print('Hello World')
+
+class KnightsTour():
+    boardSize = 6
+    knightsMoves = ((-2,1),(-1,2),(1,2),(2,1),(2,-1),(1,-2),(-1,-2),(-2,-1))
+
+    def __init__(self,boardSize):
+        self.initialize(boardSize)
+
+    def initialize(self,boardSize):
+        self.boardSize=boardSize       
+
+
+Tour = KnightsTour(8)
+print Tour.knightsMoves
