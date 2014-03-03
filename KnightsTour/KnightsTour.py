bddc930e89975aeeefcbbc06f85b17da9b8fb9f1
George-pierce <zephon.pierce@gmail.com>
2014-03-03 00:37:34 -0500

    Main logic is finished, now to debug.

--- KnightsTour/KnightsTour.py
+++ KnightsTour/KnightsTour.py
@@ -9,16 +9,38 @@ class Board():
         self.gameBoard = [[0 for x in xrange(boardSize)] for x in xrange(boardSize)]
      
     def resetPosition(self, rowIndex,columnIndex):
-        gameBoard[rowIndex][columnIndex] = '0'
+        if self.isPositionOnBoard(rowIndex,columnIndex):
+            self.gameBoard[rowIndex][columnIndex] = '0'       
 
     def setPosition(self, rowIndex, columnIndex,stepInterval):
-        self.gameBoard[rowIndex][columnIndex] = stepInterval
+        if self.isPositionOnBoard(rowIndex,columnIndex):
+            self.gameBoard[rowIndex][columnIndex] = self.visitedPositionChar
+        else:
+            io = 2
     
     def printBoard(self):
-        print('\n'.join([''.join(['{:4}'.format(item) for item in row]) for row in self.gameBoard]))
+        print('\n'.join([''.join(['{:1}'.format(item) for item in row]) for row in self.gameBoard]))
 
-    def isValidMove(row,column):
-       
+    def isValidMove(self,row,column):
+       # Is move on gameBoard?       
+       if not self.isPositionOnBoard(row,column):
+           return False        
+       #is move still valid, ie not traversed already
+       if self.gameBoard[row][column] != 0:
+           return False
+       return True
+
+    def isPositionOnBoard(self,row,column):
+       if (row < 0) or (column < 0) or (row > len(self.gameBoard)-1) or (column > len(self.gameBoard)-1):
+           return False 
+       return True
+
+    def successfulTraversal(self):
+         for row in range(len(self.gameBoard)):
+            for column in range(len(self.gameBoard[row])):
+                if str(self.gameBoard[row][column]) == '0':
+                    return False
+         return True
 
 
 class KnightsTour():
@@ -37,19 +59,26 @@ class KnightsTour():
         self.playersBoard.printBoard()
 
     def runSimulation(self,startingRow,startingColumn,stepInterval):
-        print 'You are starting at row ',startingRow, ' and column ', startingColumn
-
-        #Update the board with new position
-        self.playersBoard.setPosition(startingRow,startingColumn,1)
-
+        validMove = self.playersBoard.isValidMove(startingRow,startingColumn)
+        if validMove:
+            #Update the board with new position
+            print 'You are starting at row ',startingRow, ' and column ', startingColumn
+            self.playersBoard.setPosition(startingRow,startingColumn,1)
+            #Check if we finished the Tour
+            if self.playersBoard.successfulTraversal():
+                print "We won!!" 
+                self.printBoard()
+                time.sleep(50)
+        else:
+            return      
         #Print the current Location on the board
         self.printBoard()
         #Try each available move, and run the simulation on the new starting position
         for indexRow,indexColumn in self.knightsMoves:
-            newPosition_Row = ((startingRow)+ indexRow )
-            newPosition_Column = ((startingColumn) + indexColumn)
-            print  "starting at position ",startingRow, startingColumn, " and moving to these coords: ",  indexRow , indexColumn, " and moving to ", newPosition_Row, ' ' , newPosition_Column
-            time.sleep(10)
+            newPosition_Row = startingRow+ indexRow 
+            newPosition_Column = startingColumn + indexColumn           
+            self.runSimulation(newPosition_Row,newPosition_Column,stepInterval+1)
+            self.playersBoard.resetPosition(newPosition_Row,newPosition_Column)
 
         
 
